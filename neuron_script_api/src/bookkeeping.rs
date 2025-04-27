59f1431733e736e6fb82921b37628e5e66506329
superspeeder <andy@delusion.org>
2025-04-26 22:15:24 -0700

    fix another bug in the bookkeeping thing

--- neuron_script_api/src/bookkeeping.rs
+++ neuron_script_api/src/bookkeeping.rs
@@ -13,7 +13,7 @@ pub struct ScriptBackendRef(pub &'static mut dyn ScriptBackend);
 #[macro_export]
 macro_rules! plugin_bookkeeping {
     ($plugin_type:ident) => {
-        static mut PLUGIN: std::sync::Once<ManuallyDrop<$plugin_type>> = std::sync::Once::new();
+        static mut PLUGIN: std::sync::Once<std::mem::ManuallyDrop<$plugin_type>> = std::sync::Once::new();
         #[unsafe(no_mangle)]
         pub unsafe extern "C" fn _plugin_init(backend: *mut dyn $crate::backend::ScriptBackend) -> *mut dyn $crate::bookkeeping::Plugin {
             PLUGIN.call_once(|| std::mem::ManuallyDrop::new($plugin_type::new($crate::bookkeeping::ScriptBackendRef(unsafe { &mut *backend }))));

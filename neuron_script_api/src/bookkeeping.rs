fcf6cb46a72e8c7ab82b650aa17b5e20b95b3715
superspeeder <andy@delusion.org>
2025-04-26 22:55:55 -0700

    once again I have restructured the code in the bookkeeping to see if I can get it to work the way I need it to

--- neuron_script_api/src/bookkeeping.rs
+++ neuron_script_api/src/bookkeeping.rs
@@ -1,4 +1,8 @@
 use crate::backend::ScriptBackend;
+use std::mem::ManuallyDrop;
+use std::ops::Deref;
+use std::sync::Arc;
+use parking_lot::RwLock;
 
 pub trait Plugin {
 }
@@ -12,16 +16,20 @@ pub struct ScriptBackendRef(pub &'static mut dyn ScriptBackend);
 #[macro_export]
 macro_rules! plugin_bookkeeping {
     ($plugin_type:ident) => {
-        static mut PLUGIN: std::sync::OnceLock<std::mem::ManuallyDrop<$plugin_type>> = std::sync::OnceLock::new();
+        static mut PLUGIN: std::sync::OnceLock<*mut dyn $plugin_type> = std::sync::OnceLock::new();
         #[unsafe(no_mangle)]
         pub unsafe extern "C" fn _plugin_init(backend: *mut dyn $crate::backend::ScriptBackend) -> *mut dyn $crate::bookkeeping::Plugin {
             use std::ops::DerefMut;
-            PLUGIN.get_mut_or_init(||std::mem::ManuallyDrop::new($plugin_type::new($crate::bookkeeping::ScriptBackendRef(unsafe { &mut *backend })))).deref_mut() as *mut $plugin_type as *mut dyn $crate::bookkeeping::Plugin
+            *PLUGIN.get_or_init(||$crate::bookkeeping::PluginWrapper::new(
+                $plugin_type::new($crate::bookkeeping::ScriptBackendRef(backend))))
+            as *mut dyn $crate::bookkeeping::Plugin
         }
 
         #[unsafe(no_mangle)]
         pub unsafe extern "C" fn _plugin_terminate() {
-            if let Some(plug) = PLUGIN.get_mut() { std::mem::ManuallyDrop::drop(plug) };
+            if let Some(&plug) = PLUGIN.get() {
+                std::mem::drop(std::boxed::Box::from_raw(plug as *mut dyn $plugin_type)); // rebox the memory and immediately drop it
+            };
         }
     }
 }
